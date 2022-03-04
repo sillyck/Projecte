26134661259bea3a18a352c6a15c3b147173af63
EloySPAIN <yole.tres@gmail.com>
2022-03-04 16:58:30 +0100

    IUsuarioDAO

---
+++ src/main/java/com/example/demo/dao/IUsuarioDAO.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.dto.Usuario;
+
+
+
+
+public interface IUsuarioDAO extends JpaRepository<Usuario, Integer> {
+
+	Usuario findByUsername(String username);
+}
