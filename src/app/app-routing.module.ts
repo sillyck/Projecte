df805971d49fdcfb591fb6926f51a87dfe301734
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-03-24 11:20:18 +0100

    rutasHechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './izquierda/login/login.component';
+import { RegisterComponent } from './izquierda/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
