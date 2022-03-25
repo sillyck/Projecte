d8d0bfc48c097863f9de7391ee9b7b5988dce83b
Jordi Ribellas <ribellas2002@gmail.com>
2022-03-25 17:44:37 +0100

    maps

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  styleUrls: ['./mapa.component.css'],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `
 })
 export class MapaComponent {
 
