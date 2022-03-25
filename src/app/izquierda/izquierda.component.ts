52c4e084f0610a01e4f40b2b1c90fcbe2bd8c384
EloySPAIN <yole.tres@gmail.com>
2022-03-25 19:49:49 +0100

    Busqueda

--- src/app/izquierda/izquierda.component.ts
+++ src/app/izquierda/izquierda.component.ts
@@ -13,7 +13,7 @@ import { trigger, state, style, animate, transition, animation } from '@angular/
 		    animate('200ms', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
       transition(':leave', [
-        animate('200ms', style({ opacity: 0, transform: 'translateY(21px)' })),
+        animate('200ms', style({ opacity: 0, transform: 'translateY(10px)' })),
       ]),
     ])
   ]
