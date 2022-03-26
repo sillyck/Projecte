5b0b4584b2d5f7525691169869eefbaf2e96186b
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-03-26 19:10:47 +0100

    filtroEstrellasPuestasNoFuncionales

--- src/app/izquierda/filtros/filtros.component.ts
+++ src/app/izquierda/filtros/filtros.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
+  estrellas:number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  botonApr(n: number) {
+    this.estrellas = n;
   }
 
 }
